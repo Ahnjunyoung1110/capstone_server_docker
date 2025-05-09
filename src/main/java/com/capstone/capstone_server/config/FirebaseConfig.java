4b6b079108950bd95f3c7f968d90077bc1f31621
Seojun <auddml321@hanyang.ac.kr>
2025-05-09 16:12:15 +0900

    chore: FCM token을 위한 설정 파일과 DTO

---
+++ src/main/java/com/capstone/capstone_server/config/FirebaseConfig.java
@@ -0,0 +1,28 @@
+package com.capstone.capstone_server.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Configuration
+public class FirebaseConfig {
+
+    @PostConstruct
+    public void init() throws IOException {
+        FileInputStream serviceAccount =
+                new FileInputStream("src/main/resources/capstonealram-firebase-adminsdk-fbsvc-7c50ae5a8a.json");
+
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+        if (FirebaseApp.getApps().isEmpty()) {
+            FirebaseApp.initializeApp(options);
+        }
+    }
+}
