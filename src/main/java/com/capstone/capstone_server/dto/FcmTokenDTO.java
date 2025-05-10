dcae2e424a5082ed310d055c544badc15a38bc98
Seojun <auddml321@hanyang.ac.kr>
2025-05-10 20:01:26 +0900

    feat: FCM Token 받아서 DB에 저장하는 기능 추가

--- src/main/java/com/capstone/capstone_server/dto/FcmTokenDTO.java
+++ src/main/java/com/capstone/capstone_server/dto/FcmTokenDTO.java
@@ -1,22 +1,15 @@
 package com.capstone.capstone_server.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class FcmTokenDTO {
     private String userId;
     private String token;
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
 }
