5bb2118bec00e94ff775de24acdd641b99f1ced1
Seojun <auddml321@hanyang.ac.kr>
2025-05-10 20:01:53 +0900

    feat: 유저 로그아웃시 FCM token 처리
    로그아웃시 매핑된 Fcm Token 찾아 삭제하는 기능 추가

--- src/main/java/com/capstone/capstone_server/controller/user/NotificationController.java
+++ src/main/java/com/capstone/capstone_server/controller/user/NotificationController.java
@@ -1,6 +1,9 @@
 package com.capstone.capstone_server.controller.user;
 
+import com.capstone.capstone_server.dto.FcmLogoutDTO;
 import com.capstone.capstone_server.dto.FcmTokenDTO;
+import com.capstone.capstone_server.entity.FcmTokenEntity;
+import com.capstone.capstone_server.repository.FcmTokenRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +15,8 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class NotificationController {
 
+    private final FcmTokenRepository fcmTokenRepository;
+
     @PostMapping("/token")
     public ResponseEntity<Void> registerFcmToken(@RequestBody FcmTokenDTO request) {
         String userId = request.getUserId();
@@ -19,8 +24,35 @@ public class NotificationController {
 
         log.info("FCM 토큰 수신: userId={}, token={}", request.getUserId(), request.getToken());
 
-        // TODO: DB에 저장 또는 업데이트 로직 작성
-        // 예: fcmTokenService.save(userId, token);
+        // 이미 같은 토큰이 존재하는 경우는 저장하지 않음
+        if (fcmTokenRepository.findByToken(token).isEmpty()) {
+            FcmTokenEntity entity = new FcmTokenEntity();
+            entity.setUserId(userId);
+            entity.setToken(token);
+            fcmTokenRepository.save(entity);
+            log.info("토큰 저장 완료");
+        } else {
+            log.info("이미 등록된 토큰입니다. 저장 생략");
+        }
+
+        return ResponseEntity.ok().build();
+    }
+    //FCM 토큰 삭제 (로그아웃 시)
+    @PostMapping("/logout")
+    public ResponseEntity<Void> removeFcmToken(@RequestBody FcmLogoutDTO request) {
+        String userId = request.getUserId();
+        String token = request.getToken();
+
+        log.info("로그아웃 요청: userId={}, token={}", userId, token);
+
+        fcmTokenRepository.findByToken(token).ifPresentOrElse(existing -> {
+            if (existing.getUserId().equals(userId)) {
+                fcmTokenRepository.delete(existing);
+                log.info("FCM 토큰 삭제 완료");
+            } else {
+                log.warn("유저 ID 불일치하여 삭제하지 않음");
+            }
+        }, () -> log.warn("존재하지 않는 토큰입니다. 삭제 스킵"));
 
         return ResponseEntity.ok().build();
     }
---
+++ src/main/java/com/capstone/capstone_server/dto/FcmLogoutDTO.java
@@ -0,0 +1,13 @@
+package com.capstone.capstone_server.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class FcmLogoutDTO {
+    private String userId;
+    private String token;
+}
