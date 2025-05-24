3b4ffbea367ea7d1afb18ad5c230719e93a62f25
Ahnjunyoung1110 <69619611+Ahnjunyoung1110@users.noreply.github.com>
2025-05-25 01:36:50 +0900

    Update FirebaseConfig.java

--- src/main/java/com/capstone/capstone_server/config/FirebaseConfig.java
+++ src/main/java/com/capstone/capstone_server/config/FirebaseConfig.java
@@ -6,7 +6,7 @@ import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.FileInputStream;
+import java.io.InputStream;
 import java.io.IOException;
 
 @Configuration
